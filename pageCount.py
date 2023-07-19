0f853b036d992be60731c79e0064b05c549fbb06
Purushothaman2004 <139734832+Purushothaman2004@users.noreply.github.com>
2023-07-19 16:26:13 +0530

    Create pageCount.py

---
+++ pageCount.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER p
+#
+
+def pageCount(n, p):
+    last_page = n if n % 2 else n+1
+    if p in (1, last_page, n):
+        return 0
+    left = p // 2
+    right = (last_page-p) // 2
+    return min(left, right)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = int(input().strip())
+
+    result = pageCount(n, p)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
