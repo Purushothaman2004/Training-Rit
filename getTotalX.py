d005c8d9c52fe889f549e72291254c216bb997dc
Purushothaman2004 <139734832+Purushothaman2004@users.noreply.github.com>
2023-07-19 16:24:52 +0530

    Create getTotalX.py

---
+++ getTotalX.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def getTotalX(a, b):
+    factors = list(range(1, min(b) + 1))
+    
+    for element in a:
+        factors = [
+            factor for factor in factors if factor % element == 0
+            ]
+    
+    for element in b:
+        factors = [
+            factor for factor in factors if element % factor == 0
+            ]
+
+    return len(factors)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
